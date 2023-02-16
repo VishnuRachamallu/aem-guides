3696273c37b5a431378772347747c37c0714701a
Vishnu Rachamallu <vitturachamallu001@gmail.com>
2023-02-16 12:26:47 +0530

    first command

---
+++ core/src/main/java/com/adobe/aem/guides/core/services/DemoService.java
@@ -0,0 +1,14 @@
+package com.adobe.aem.guides.core.services;
+
+import java.util.Iterator;
+
+import com.day.cq.wcm.api.Page;
+
+public interface DemoService {
+    public String getPages();
+
+
+}
+
+
+
